b1338376f86a4c8edd6bd90fafd9e4545e56b948
asif-kamal <asif.kamal1489@gmail.com>
2024-11-04 13:17:47 -0500

    added correct table name and escaped column names to match DB

--- src/main/java/com/electronics/pgdata/model/Electronic.java
+++ src/main/java/com/electronics/pgdata/model/Electronic.java
@@ -1,65 +1,79 @@
 package com.electronics.pgdata.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-import org.hibernate.annotations.Type;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Entity
-@Table(name = "electronics")
+@Table(name = "cleaneddata02_electronics")
 public class Electronic {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "asins")
+    private String asins;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "categories")
+    private String categories;
+
+    @Column(name = "\"dateAdded\"")
+    private String dateAdded;
+
+    @Column(name = "\"dateUpdated\"")
+    private String dateUpdated;
+
+    @Column(name = "ean")
+    private Float ean;
+
+    @Column(name = "\"imageURLs\"")
+    private String imageURLs;
+
+    @Column(name = "keys")
+    private String keys;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    @Column(name = "\"manufacturerNumber\"")
+    private String manufacturerNumber;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "\"primaryCategories\"")
+    private String primaryCategories;
+
+    @Column(name = "\"sourceURLs\"")
+    private String sourceURLs;
+
+    @Column(name = "upc")
+    private String upc;
+
+    @Column(name = "weight")
+    private String weight;
+
     @Embedded
     @AttributeOverrides({
-        @AttributeOverride(name = "amountMax", column = @Column(name = "prices_amount_max")),
-        @AttributeOverride(name = "amountMin", column = @Column(name = "prices_amount_min")),
-        @AttributeOverride(name = "availability", column = @Column(name = "prices_availability")),
-        @AttributeOverride(name = "condition", column = @Column(name = "prices_condition")),
-        @AttributeOverride(name = "currency", column = @Column(name = "prices_currency")),
-        @AttributeOverride(name = "dateSeen", column = @Column(name = "prices_date_seen")),
-        @AttributeOverride(name = "isSale", column = @Column(name = "prices_is_sale")),
-        @AttributeOverride(name = "merchant", column = @Column(name = "prices_merchant")),
-        @AttributeOverride(name = "shipping", column = @Column(name = "prices_shipping")),
-        @AttributeOverride(name = "sourceURLs", column = @Column(name = "prices_source_urls"))
+        @AttributeOverride(name = "amountMax", column = @Column(name = "prices.amountMax")),
+        @AttributeOverride(name = "amountMin", column = @Column(name = "prices.amountMin")),
+        @AttributeOverride(name = "availability", column = @Column(name = "prices.availability")),
+        @AttributeOverride(name = "condition", column = @Column(name = "prices.condition")),
+        @AttributeOverride(name = "currency", column = @Column(name = "prices.currency")),
+        @AttributeOverride(name = "dateSeen", column = @Column(name = "prices.dateSeen")),
+        @AttributeOverride(name = "isSale", column = @Column(name = "prices.isSale")),
+        @AttributeOverride(name = "merchant", column = @Column(name = "prices.merchant")),
+        @AttributeOverride(name = "shipping", column = @Column(name = "prices.shipping")),
+        @AttributeOverride(name = "sourceURLs", column = @Column(name = "prices.sourceURLs"))
     })
-    private Price prices;
-    
-    private String asins;
-    private String brand;
-    
-    @Column(columnDefinition = "text[]")
-    private String[] categories;
-    
-    @Column(name = "date_added")
-    private LocalDateTime dateAdded;
-    
-    @Column(name = "date_updated")
-    private LocalDateTime dateUpdated;
-    
-    private String ean;
-    
-    @Column(name = "image_urls", columnDefinition = "text[]")
-    private String[] imageURLs;
-    
-    private String keys;
-    private String manufacturer;
-    
-    @Column(name = "manufacturer_number")
-    private String manufacturerNumber;
-    
-    private String name;
-    
-    @Column(name = "primary_categories", columnDefinition = "text[]")
-    private String[] primaryCategories;
-    
-    @Column(name = "source_urls", columnDefinition = "text[]")
-    private String[] sourceURLs;
-    
-    private String upc;
-    private String weight;
+    private ProductPrice prices;
 }
