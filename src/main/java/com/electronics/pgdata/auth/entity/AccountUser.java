39cd63ed8e99b851b45e3b85008c511e58cbb22f
asif-kamal <asif.kamal1489@gmail.com>
2025-09-23 14:10:04 -0500

    added fields and unimplemented methods

--- src/main/java/com/electronics/pgdata/auth/entity/AccountUser.java
+++ src/main/java/com/electronics/pgdata/auth/entity/AccountUser.java
@@ -1,4 +1,63 @@
 package com.electronics.pgdata.auth.entity;
 
-public class AccountUser {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Table(name = "AUTH_USER_DETAILS")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountUser implements UserDetails {
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    private String firstName;
+
+    private String lastName;
+
+    @JsonIgnore
+    private String password;
+
+    private Date createdOn;
+
+    private Date updatedOn;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+
+    private String provider;
+
+    private String verificationCode;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
