c44a84be6776e2a69d72bc9b62b89cd8ddc0247d
asif-kamal <asif.kamal1489@gmail.com>
2025-09-24 18:53:23 -0500

    defined 2 new DTO classes

--- src/main/java/com/electronics/pgdata/auth/controller/AuthController.java
+++ src/main/java/com/electronics/pgdata/auth/controller/AuthController.java
@@ -1,7 +1,19 @@
 package com.electronics.pgdata.auth.controller;
 
+import com.electronics.pgdata.auth.dto.AccountUserToken;
+import com.electronics.pgdata.auth.dto.LoginRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<AccountUserToken> login(@RequestBody LoginRequest loginRequest){
+
+    }
 }
---
+++ src/main/java/com/electronics/pgdata/auth/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.electronics.pgdata.auth.dto;
+
+public class LoginRequest {
+}
