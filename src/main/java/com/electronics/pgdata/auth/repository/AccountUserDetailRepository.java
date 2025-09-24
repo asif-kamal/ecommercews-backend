7554d013af62378fa9307ab8ebdcca592b3fe6e7
asif-kamal <asif.kamal1489@gmail.com>
2025-09-23 20:09:54 -0500

    defined loadUserByUsername()

--- src/main/java/com/electronics/pgdata/auth/repository/AccountUserDetailRepository.java
+++ src/main/java/com/electronics/pgdata/auth/repository/AccountUserDetailRepository.java
@@ -2,6 +2,9 @@ package com.electronics.pgdata.auth.repository;
 
 import com.electronics.pgdata.auth.entity.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountUserDetailRepository extends JpaRepository<AccountUser, Long> {
+    AccountUser findByEmail(String username);
 }
--- src/main/java/com/electronics/pgdata/auth/service/CustomAccountUserDetailService.java
+++ src/main/java/com/electronics/pgdata/auth/service/CustomAccountUserDetailService.java
@@ -1,12 +1,24 @@
 package com.electronics.pgdata.auth.service;
 
+import com.electronics.pgdata.auth.entity.AccountUser;
+import com.electronics.pgdata.auth.repository.AccountUserDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomAccountUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private AccountUserDetailRepository accountUserDetailRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        AccountUser aUser = accountUserDetailRepository.findByEmail(username);
+        if (aUser == null) {
+            throw new UsernameNotFoundException("User not found by this username: " + username);
+        }
+        return aUser;
     }
 }
