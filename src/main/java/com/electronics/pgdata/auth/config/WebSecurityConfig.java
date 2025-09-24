e6e45671a500f4385bfaef5cc85a278d5cb044a9
asif-kamal <asif.kamal1489@gmail.com>
2025-09-23 19:59:50 -0500

    added passwordEncoder() and authenticationConfiguration()

--- src/main/java/com/electronics/pgdata/auth/config/WebSecurityConfig.java
+++ src/main/java/com/electronics/pgdata/auth/config/WebSecurityConfig.java
@@ -3,8 +3,13 @@ package com.electronics.pgdata.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +22,16 @@ public class WebSecurityConfig {
                 .requestMatchers(HttpMethod.GET,"/api/electronics").permitAll()
                 .anyRequest().authenticated());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationConfiguration authenticationConfiguration() {
+        AuthenticationConfiguration authenticationConfiguration = new AuthenticationConfiguration();
+        return null;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
