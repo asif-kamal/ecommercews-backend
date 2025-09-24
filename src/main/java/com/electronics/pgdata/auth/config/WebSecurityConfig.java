b6476c71da33b299e7cd3837f3f26deb8b9f0f53
asif-kamal <asif.kamal1489@gmail.com>
2025-09-23 20:17:08 -0500

    defined authenticationManager w/o using DaoAuthenticationProvider since deprecated

--- src/main/java/com/electronics/pgdata/auth/config/WebSecurityConfig.java
+++ src/main/java/com/electronics/pgdata/auth/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.electronics.pgdata.auth.config;
 
+import com.electronics.pgdata.auth.service.CustomAccountUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +18,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
@@ -25,9 +32,8 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public AuthenticationConfiguration authenticationConfiguration() {
-        AuthenticationConfiguration authenticationConfiguration = new AuthenticationConfiguration();
-        return null;
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
