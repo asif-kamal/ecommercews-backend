bafaff740792fb6966568254c9cfe8183099ef9f
asif-kamal <asif.kamal1489@gmail.com>
2025-09-25 19:23:04 -0500

    extending JpaRepository

--- src/main/java/com/electronics/pgdata/repository/AuthorityRepository.java
+++ src/main/java/com/electronics/pgdata/repository/AuthorityRepository.java
@@ -1,7 +1,11 @@
 package com.electronics.pgdata.repository;
 
+import com.electronics.pgdata.auth.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface AuthorityRepository {
+public interface AuthorityRepository extends JpaRepository<Authority, UUID> {
 }
