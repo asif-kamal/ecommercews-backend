69a3b5bf49578022abb6f4c4005d2a0627b7dea9
asif-kamal <asif.kamal1489@gmail.com>
2025-09-26 19:07:22 -0500

    assign Authority instance by role code

--- src/main/java/com/electronics/pgdata/service/AuthorityService.java
+++ src/main/java/com/electronics/pgdata/service/AuthorityService.java
@@ -1,6 +1,8 @@
 package com.electronics.pgdata.service;
 
 import com.electronics.pgdata.auth.entity.Authority;
+import com.electronics.pgdata.repository.AuthorityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,8 +11,13 @@ import java.util.List;
 @Service
 public class AuthorityService {
 
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
     public List<Authority> getAccountUserAuthorities() {
         List<Authority> authorities = new ArrayList<>();
+        Authority authority = authorityRepository.findByRoleCode("ROLE_USER");
 
+        return authorities;
     }
 }
