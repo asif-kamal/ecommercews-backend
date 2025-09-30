b38b158d29ffe76bcb5e48a61ce6fc4aadc1b4e5
asif-kamal <asif.kamal1489@gmail.com>
2025-09-30 14:05:16 -0500

    set mailMessage to proper inputs

--- src/main/java/com/electronics/pgdata/service/EmailService.java
+++ src/main/java/com/electronics/pgdata/service/EmailService.java
@@ -3,6 +3,7 @@ package com.electronics.pgdata.service;
 import com.electronics.pgdata.auth.entity.AccountUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +14,29 @@ public class EmailService {
     private JavaMailSender mailSender;
     @Value("${spring.mail.username}")
     private String sender;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     public String sendEmail(AccountUser accountUser) {
         String subject = "Welcome to Eccomercews. Please verify your email.";
         String senderName = "Eccomercews Support";
         String mailContent = "<p>Dear " + accountUser.getFirstName() + ", your verification code is: "
                 + accountUser.getVerificationCode()+ "</p>";
+        mailContent += "<br />";
         mailContent += "<p>Please enter this code to verify your account.</p>";
 
+        try {
 
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(accountUser.getEmail());
+            mailMessage.setText(mailContent);
+            mailMessage.setSubject(subject);
+            javaMailSender.send(mailMessage);
+        } catch (Exception e) {
+            return "Error while sending mail ..";
+
+        }
+        return "Mail Sent Success!";
     }
 }
