cbb30d856d9abbfd96f3212af700c389613f2e35
asif-kamal <asif.kamal1489@gmail.com>
2025-10-01 13:50:25 -0500

    built UserDetailsDTO for profile

--- src/main/java/com/electronics/pgdata/auth/dto/UserDetailsDTO.java
+++ src/main/java/com/electronics/pgdata/auth/dto/UserDetailsDTO.java
@@ -1,6 +1,23 @@
 package com.electronics.pgdata.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDetailsDTO {
 
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private Object authorities;
 
 }
