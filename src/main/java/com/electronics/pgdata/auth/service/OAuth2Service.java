ad203e54d1dd6409e89f2ba7693301f787ef60f0
asif-kamal <asif.kamal1489@gmail.com>
2025-10-02 15:01:23 -0500

    created getUser() and createUser() for Provider login

--- src/main/java/com/electronics/pgdata/auth/service/OAuth2Service.java
+++ src/main/java/com/electronics/pgdata/auth/service/OAuth2Service.java
@@ -1,14 +1,34 @@
 package com.electronics.pgdata.auth.service;
 
 import com.electronics.pgdata.auth.entity.AccountUser;
+import com.electronics.pgdata.auth.repository.AccountUserDetailRepository;
+import com.electronics.pgdata.service.AuthorityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OAuth2Service {
+
+    @Autowired
+    AccountUserDetailRepository accountUserDetailRepository;
+
+    @Autowired
+    private AuthorityService authorityService;
+
     public AccountUser getUser(String username) {
+        return accountUserDetailRepository.findByEmail(username);
     }
 
-    public AccountUser createUser(OAuth2User principal, String google) {
+    public AccountUser createUser(OAuth2User principal, String provider) {
+        AccountUser accountUser = AccountUser.builder()
+                .firstName(principal.getAttribute("given_name"))
+                .lastName(principal.getAttribute("family_name"))
+                .email(principal.getAttribute("email"))
+                .provider(provider)
+                .enabled(true)
+                .authorities(authorityService.getAccountUserAuthorities())
+                .build();
+        return accountUserDetailRepository.save(accountUser);
     }
 }
