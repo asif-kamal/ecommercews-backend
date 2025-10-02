eb6c294b43b8062aa1fc1f9b536118a64a442463
asif-kamal <asif.kamal1489@gmail.com>
2025-10-02 13:11:49 -0500

    moved controller and service class under auth package

--- src/main/java/com/electronics/pgdata/auth/controller/Oauth2Controller.java
+++ src/main/java/com/electronics/pgdata/auth/controller/Oauth2Controller.java
@@ -1,6 +1,10 @@
 package com.electronics.pgdata.auth.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/oauth2")
 public class Oauth2Controller {
+
+    @GetMapping("/success")
+    public void oauth2Success(@AuthenticationPrincipal OAuth2User principal, HttpServletResponse response) {
+        String username = principal.getAttribute("email");
 }
---
+++ src/main/java/com/electronics/pgdata/auth/service/OAuth2Service.java
@@ -0,0 +1,4 @@
+package com.electronics.pgdata.auth.service;
+
+public class OAuth2Service {
+}
