e5f60775e4e2a469deafde12adb7b90cbc4422ba
asif-kamal <asif.kamal1489@gmail.com>
2025-10-05 14:26:44 -0500

    added Vercel domain url to CORS

--- src/main/java/com/electronics/pgdata/auth/config/WebSecurityConfig.java
+++ src/main/java/com/electronics/pgdata/auth/config/WebSecurityConfig.java
@@ -104,7 +104,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*", "https://ecommercews-frontend-18cm.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
